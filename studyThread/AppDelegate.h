e59c1e8b5e922f43fe5b566dde44f8152353de28
韩钊 <hanzhao@xiyoudewll.local>
2016-09-12 21:27:00 +0800

    Initial Commit

---
+++ studyThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  studyThread
+//
+//  Created by 韩钊 on 16/9/12.
+//  Copyright © 2016年 韩钊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ studyThread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studyThread
+//
+//  Created by 韩钊 on 16/9/12.
+//  Copyright © 2016年 韩钊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
